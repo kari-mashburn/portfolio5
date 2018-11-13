b9cc6ca37709317b3ebd0014d466c82f2d2daa79
Kari Mashburn <kmashburn@fullsail.edu>
2018-11-13 09:41:28 -0500

    updates to browserrouter

--- mystars 2/src/App.js
+++ mystars 2/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import {HashRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename="/">
         <div>
           <Header />
           <Main />
